46bf466d8434edaafab36181f4dbdeb07a033d8b
Daniel Kafer <daniel.j.kafer@gmail.com>
2016-10-25 00:02:37 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var express = require('express');
+var socket_io = require('socket.io');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = http.Server(app);
+var io = socket_io(server);
+
+io.on('connection', function(socket) {
+	console.log('A user has connected');
+	
+	socket.on('draw', function(position) {
+		socket.broadcast.emit('draw', position);
+	});
+	
+	socket.on('guess', function(guess) {
+		socket.broadcast.emit('guess', guess);
+	});
+	
+	socket.on('disconnect', function() {
+		console.log('A user has disconnected');
+	});
+
+});
+
+
+server.listen(process.env.PORT || 8080);
